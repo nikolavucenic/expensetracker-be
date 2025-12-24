f55cb912abaa0a04f3788661b74cb5bad1fb6f6b
nikolavucenic <nikolavucenic01@gmail.com>
2025-12-24 12:59:47 +0100

    Fix too many requests issue

--- src/main/kotlin/com/nv/expensetracker/security/JwtAuthFilter.kt
+++ src/main/kotlin/com/nv/expensetracker/security/JwtAuthFilter.kt
@@ -11,25 +11,36 @@ import org.springframework.web.filter.OncePerRequestFilter
 @Component
 class JwtAuthFilter(
     private val jwtService: JwtService
-): OncePerRequestFilter() {
+) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        if (!request.servletPath.startsWith("/auth")) {
-            val authHeader = request.getHeader("Authorization")
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                if (jwtService.validateAccessToken(authHeader)) {
-                    val userId = jwtService.getUserIdFromToken(authHeader)
-                    val auth = UsernamePasswordAuthenticationToken(userId, null, emptyList())
-                    SecurityContextHolder.getContext().authentication = auth
-                }
+        if (request.servletPath.startsWith("/auth")) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val authHeader = request.getHeader("Authorization")
+
+        if (authHeader?.startsWith("Bearer ") == true) {
+            val token = authHeader.removePrefix("Bearer ").trim()
+
+            if (jwtService.validateAccessToken(token)) {
+                val userId = jwtService.getUserIdFromToken(token)
+
+                val authentication = UsernamePasswordAuthenticationToken(
+                    userId,
+                    null,
+                    emptyList()
+                )
+
+                SecurityContextHolder.getContext().authentication = authentication
             }
         }
 
         filterChain.doFilter(request, response)
     }
-
 }
